101c7f8057a1949994410528f3850b535a129d68
melissavoirol <melissa.voirol@he-arc.ch>
2015-06-24 17:03:50 +0200

    Charts

---
+++ scripts/ecff4a05.main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){"use strict";$("a").smoothScroll();var a=[{value:33,color:"#F7464A",highlight:"#FF5A5E",label:"Red"}],b=document.getElementById("skills").getContext("2d");new Chart(b).Doughnut(a)});
